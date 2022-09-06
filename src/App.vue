9eb7afd6799c36ce8e344c8e384e907c54901cc3
HsiaooooooJu <kelllllyko@gmail.com>
2022-09-06 16:34:25 +0800

    Install Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -30,3 +30,12 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+// 表示要載入 Bootstrap 的 JS，因為在 node_module/bootstrap/package.json 的 main 中，已有指定預設路徑為 dist/js/bootstrap，所以可以簡寫，但 CSS 的部分反而就找不到了，所以要寫出完整路徑
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
+}
+</script>
