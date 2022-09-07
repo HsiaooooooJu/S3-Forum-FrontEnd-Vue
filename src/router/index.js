673f4811014d7098eeea6a973d3437a3a159f2dc
HsiaooooooJu <kelllllyko@gmail.com>
2022-09-07 10:49:53 +0800

    Add page - SignIn

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue' // 所有的路由都匹配不到的情況
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue')
+  },
   {
     // Vue Router 路由會由上往下進行匹配，所以當路由一條條解析下來，所有的路由都無法匹配，最後就會匹配到 path: '*'，代表「所有的網址」，管使用者輸入什麼，只要找不到對應網頁，最後就會返回 NotFound 這個頁面 (也代表要放在路由列表的最後一個)
     path: '*',
---
+++ src/views/SignIn.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container py-5">
+    <form @submit.prevent.stop="handleSubmit" class="w-100">
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">
+          Sign In
+        </h1>
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="email">Email</label>
+        <input v-model="email" id="email" name="email" type="email" class="form-control" placeholder="Email"
+          autocomplete="username" required autofocus>
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">Password</label>
+        <input v-model="password" id="password" name="password" type="password" class="form-control"
+          placeholder="Password" autocomplete="current-password" required>
+      </div>
+
+      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">
+        Submit
+      </button>
+
+      <div class="text-center mb-3">
+        <p>
+          <router-link to="/signup">Sign Up</router-link>
+        </p>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">
+        &copy; 2022-2023
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  // Data must be a function to keep a component’s data separate for each initialization.
+  // Methods and such stay the same.
+  // Vue avoids error happen by having data be a function that returns an object.
+  data() {
+    // A function needs to be returned
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const data = JSON.stringify({
+        emial: this.email,
+        password: this.password
+      })
+      // TODO: 向後端驗證使用者登入資訊是否合法
+      console.log('data', data)
+    }
+  }
+}
+</script>
+
