9d83741c0fbfb48355b82d9d024f6114807169ed
HsiaooooooJu <kelllllyko@gmail.com>
2022-09-11 18:17:09 +0800

    Add feature: - show image preview in AdminRestaurantForm - use FormData in AdminRestaurantForm

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.stop.prevent="handleSubmit">
     <div class="form-group">
       <label for="name">Name</label>
       <input v-model="restaurant.name" id="name" type="text" class="form-control" name="name" placeholder="Enter name"
@@ -42,7 +42,14 @@
 
     <div class="form-group">
       <label for="image">Image</label>
-      <input id="image" type="file" name="image" accept="image/*" class="form-control-file">
+
+      <!-- 加入一個 <img> 元素，顯示暫存圖片 -->
+      <!-- 使用 :src="restaurant.image" 綁定暫存網址 -->
+      <img v-if="restaurant.image" :src="restaurant.image" class="d-block img-thumbnail mb-3" width="200" height="200">
+
+      <!-- <input type="file"> 是 HTML5 提供的檔案上傳功能，會在瀏覽器上呈現出一個互動介面，讓使用者可以上傳檔案，上傳的檔案會轉換成一個叫 FileList 的 DOM 物件列表，存放在 files 屬性裡 -->
+      <input @change="handleFileChange" id="image" type="file" name="image" accept="image/*" class="form-control-file">
+
     </div>
 
     <button type="submit" class="btn btn-primary">
@@ -104,6 +111,38 @@ export default {
   methods: {
     fetchCategories() {
       this.categories = dummyData.categories
+    },
+    handleFileChange(e) {
+      const files = e.target.files
+      // console.log('files', files)
+
+      if (files.length === 0) {
+        // 使用者沒有選擇上傳的檔案
+        this.restaurant.image = ''
+
+      } else {
+        // 否則產生預覽圖
+        // 使用 window.URL.createObjectURL() 為本機檔案來產生一個暫存的 URL
+        const imageURL = window.URL.createObjectURL(files[0])
+        this.restaurant.image = imageURL
+      }
+    },
+    handleSubmit(e) {
+      // 把 <form> 的內容轉換成一個物件，傳送給後端
+
+      // 透過 e.target 取得表單本身
+      const form = e.target
+
+      // 透過 new FormData(form) 產生物件實例，存在 formData 裡
+      const formData = new FormData(form)
+
+      // 透過 $emit 發送 after-submit 的事件到父層 AdminRestaurantNew，並攜帶表單資料：
+      this.$emit('after-submit', formData)
+
+      // 可以透過 entries() 方法逐條列出表單的欄位和值，取得表單的內容
+      // for (let [name, value] of formData.entries()) {
+      //   console.log(name + ': ' + value)
+      // }
     }
   }
 }
--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm />
+    <AdminRestaurantForm @after-submit="afterSubmit" />
   </div>
 </template>
 
@@ -12,6 +12,15 @@ export default {
   name: 'AdminRestaurantNew',
   components: {
     AdminRestaurantForm,
+  },
+  methods: {
+    afterSubmit(formData) {
+      // 透過 API 將表單資料送到伺服器
+      // 先用 console 確定資料有成功傳上來
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ': ' + value)
+      }
+    }
   }
 }
 </script>
