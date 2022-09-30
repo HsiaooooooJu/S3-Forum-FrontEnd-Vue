4a3b1c3778692e6570572129d89ebc660a657fbd
HsiaoJu Ko <hsiaojuko@HsiaoJus-iMac.local>
2022-09-30 11:19:04 +0800

    Fix: integrate API of restuarants.getRestuarant in RestaurantDashboard

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -21,85 +21,10 @@
 </template>
 
 <script>
+import restaurantsAPI from './../apis/restaurants'
+import { Toast } from './../utils/helpers'
 
-const dummyData = {
-  "restaurant": {
-    "id": 1,
-    "name": "Laury Gerlach",
-    "tel": "(362) 086-0126 x4590",
-    "address": "550 Walter Fields",
-    "opening_hours": "08:00",
-    "description": "enim apsxapsock",
-    "image": "https://loremflickr.com/320/240/restaurant,food/?random=84.67624997722729",
-    "viewCounts": 0,
-    "createdAt": "2022-07-30T09:11:32.000Z",
-    "updatedAt": "2022-08-12T16:17:50.000Z",
-    "CategoryId": 3,
-    "Category": {
-      "id": 3,
-      "name": "義大利料理",
-      "createdAt": "2022-07-30T09:11:32.000Z",
-      "updatedAt": "2022-07-30T09:11:32.000Z"
-    },
-    "Comments": [
-      {
-        "id": 1,
-        "text": "Illo ex quis repellat tenetur corporis consequatur omnis maxime nobis.",
-        "UserId": 3,
-        "RestaurantId": 1,
-        "createdAt": "2022-07-30T09:11:32.000Z",
-        "updatedAt": "2022-07-30T09:11:32.000Z",
-        "User": {
-          "id": 3,
-          "name": "user2",
-          "email": "user2@example.com",
-          "password": "$2a$10$/mJWLJyJpvonEmG2jp4wb.vSuFYRKGcDnF6k..6sT4CNeDnIb7PhC",
-          "isAdmin": false,
-          "image": null,
-          "createdAt": "2022-07-30T09:11:32.000Z",
-          "updatedAt": "2022-07-30T09:11:32.000Z"
-        }
-      },
-      {
-        "id": 51,
-        "text": "Suscipit rerum ut est voluptates ipsam qui minima earum.",
-        "UserId": 2,
-        "RestaurantId": 1,
-        "createdAt": "2022-07-30T09:11:32.000Z",
-        "updatedAt": "2022-07-30T09:11:32.000Z",
-        "User": {
-          "id": 2,
-          "name": "user1",
-          "email": "user1@example.com",
-          "password": "$2a$10$4uOer1U8UxL1jv7IWjrnged.7ienQ..l2MKiz2WW8p5GT/juzIrG2",
-          "isAdmin": false,
-          "image": null,
-          "createdAt": "2022-07-30T09:11:32.000Z",
-          "updatedAt": "2022-07-30T09:11:32.000Z"
-        }
-      },
-      {
-        "id": 101,
-        "text": "Quia officiis libero laborum ad ut ullam.",
-        "UserId": 3,
-        "RestaurantId": 1,
-        "createdAt": "2022-07-30T09:11:32.000Z",
-        "updatedAt": "2022-07-30T09:11:32.000Z",
-        "User": {
-          "id": 3,
-          "name": "user2",
-          "email": "user2@example.com",
-          "password": "$2a$10$/mJWLJyJpvonEmG2jp4wb.vSuFYRKGcDnF6k..6sT4CNeDnIb7PhC",
-          "isAdmin": false,
-          "image": null,
-          "createdAt": "2022-07-30T09:11:32.000Z",
-          "updatedAt": "2022-07-30T09:11:32.000Z"
-        }
-      }
-    ]
-  }
-}
-
+// import { mapState } from 'vuex'
 export default {
   name: 'RestaurantDashboard',
   data() {
@@ -113,16 +38,32 @@ export default {
       }
     }
   },
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
+  },
   methods: {
-    fetchRestaurant() {
-      const { restaurant } = dummyData
-      const { id, name, Category, Comments, viewCounts } = restaurant
-      this.restaurant = {
-        id,
-        name,
-        categoryName: Category ? Category.name : '未分類',
-        commentsLength: Comments.length,
-        viewCounts,
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data, status, statusText  } = await restaurantsAPI.getRestaurant({ restaurantId })
+
+        if(status === 'error') {
+          throw new Error(statusText)
+        }
+        const { id, name, Category, Comments, viewCounts } = data.restaurant
+        this.restaurant = {
+          id,
+          name,
+          categoryName: Category ? Category.name : '未分類',
+          commentsLength: Comments.length,
+          viewCounts,
+        }
+      } catch(error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
     }
   },
